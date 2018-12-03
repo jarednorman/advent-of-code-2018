db2e297bda1e965ee5fd6086f2277cf389642c9d
Jared Norman <jared@super.gd>
2018-12-02 20:40:30 -0800

    How about a skeleton?

--- 03.rb
+++ 03.rb
@@ -1,3 +1,26 @@
 #!/usr/bin/env ruby
 require_relative 'bootstrap'
-puts "yeah"
+require_relative '03/input.rb'
+
+class DayThree
+  def initialize(input)
+    @input = input
+  end
+
+  def part_one
+    input
+  end
+
+  def part_two
+    input
+  end
+
+  private
+
+  attr_reader :input
+end
+
+solution = DayThree.new(INPUT)
+
+puts "Part one: #{solution.part_one}"
+puts "Part two: #{solution.part_two}"
