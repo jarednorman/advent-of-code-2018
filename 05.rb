2e3b3b87e29f9090af435baf168c6541d98d1c6a
Jared Norman <jared@super.gd>
2018-12-04 21:18:57 -0800

    Part one

--- 05.rb
+++ 05.rb
@@ -1,15 +1,24 @@
 #!/usr/bin/env ruby
 require_relative 'bootstrap'
 
-INPUT = File.open('05_input.txt').read.lines.map(&:chomp)
+INPUT = File.open('05_input.txt').read.chomp
 
 class PartOne
+  BAD_COMBOS = ('a'..'z').flat_map{|l|[[l,l.capitalize].join,[l.capitalize,l].join]}
+
   def initialize(input)
     @input = input
   end
 
   def answer
-    input
+    new_length = 99999999999999999999
+
+    while new_length != input.length
+      new_length = input.length
+      BAD_COMBOS.each{|x|input.gsub!(x, '')}
+    end
+
+    input.length
   end
 
   attr_reader :input
