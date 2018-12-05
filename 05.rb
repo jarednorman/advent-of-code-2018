f26e7c6d8bb3ace8827311a8fa01103dcf21628d
Jared Norman <jared@super.gd>
2018-12-04 21:31:04 -0800

    Done!

--- 05.rb
+++ 05.rb
@@ -3,9 +3,9 @@ require_relative 'bootstrap'
 
 INPUT = File.open('05_input.txt').read.chomp
 
-class PartOne
-  BAD_COMBOS = ('a'..'z').flat_map{|l|[[l,l.capitalize].join,[l.capitalize,l].join]}
+BAD_COMBOS = ('a'..'z').flat_map{|l|[[l,l.capitalize].join,[l.capitalize,l].join]}
 
+class PartOne
   def initialize(input)
     @input = input
   end
@@ -24,4 +24,40 @@ class PartOne
   attr_reader :input
 end
 
-puts "Part one: #{PartOne.new(INPUT).answer}"
+# puts "Part one: #{PartOne.new(INPUT).answer}"
+
+class PartOneNoMuties
+  def initialize(input)
+    @input = input
+  end
+
+  def answer
+    new_length = 99999999999999999999
+
+    while new_length != input.length
+      new_length = input.length
+      BAD_COMBOS.each{|x|self.input = input.gsub(x, '')}
+    end
+
+    input.length
+  end
+
+  attr_accessor :input
+end
+
+class PartTwo
+  def initialize(input)
+    @input = input
+  end
+
+  def answer
+    ('a'..'z').map do |x|
+      i = input.gsub(x, '').gsub(x.upcase, '')
+      PartOneNoMuties.new(i).answer
+    end.min
+  end
+
+  attr_reader :input
+end
+
+puts "Part two: #{PartTwo.new(INPUT).answer}"
