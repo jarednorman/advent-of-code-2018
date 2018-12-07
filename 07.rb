daf2634f17d9078bc483be903a5fe70f23408916
Jared Norman <jared@super.gd>
2018-12-06 21:47:57 -0800

    Part two

--- 07.rb
+++ 07.rb
@@ -8,13 +8,19 @@ INPUT = File.open('07.txt').read.lines.map(&:chomp).map do |line|
   Constraint.new(step, dependency)
 end
 
+LETTERS = ('A'..'Z').to_a
+
 Step = Struct.new(:letter) do
   def dependencies
     @dependencies ||= Set.new
   end
+
+  def length
+    @length ||= LETTERS.index(letter) + 61
+  end
 end
 
-steps = ('A'..'Z').map do |letter|
+steps = LETTERS.map do |letter|
   [letter, Step.new(letter)]
 end.to_h
 
@@ -39,3 +45,56 @@ steps = steps.values
 # end
 
 # puts "Part One: #{order.join}"
+
+class Worker
+  def initialize(steps)
+    @steps = steps
+    @time = 0
+    @end_time = nil
+    @job = nil
+  end
+
+  def step!
+    if time == end_time
+      self.end_time = nil
+      steps.each do |step|
+        step.dependencies.delete(job.letter)
+      end
+      self.job = nil
+    end
+
+    start_a_job! if idle?
+
+    @time += 1
+  end
+
+  def idle?
+    @job.nil?
+  end
+
+  attr_reader :time, :steps
+  attr_accessor :job, :end_time
+
+  def start_a_job!
+    self.job = steps.find{|j|j.dependencies.empty?}
+    return unless job
+    steps.delete(job)
+    self.end_time = time + job.length
+  end
+end
+
+# 0 1 2 3 4
+# A A A
+
+workers = 5.times.map do
+  Worker.new(steps)
+end
+
+loop do
+  workers.each(&:step!)
+
+  if workers.all?(&:idle?)
+    puts "Part Two: #{workers.first.time - 1}"
+    break
+  end
+end
