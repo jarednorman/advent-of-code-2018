d0b463f105e0e7df8425bd486fef884e5d2a16e1
Jared Norman <jared@super.gd>
2018-12-14 22:34:46 -0800

    Coming along

--- 13.rb
+++ 13.rb
@@ -1,4 +1,84 @@
 #!/usr/bin/env ruby
 require_relative 'bootstrap'
 
-puts "yeah"
+NOTHING = ' '
+INTERSECTION = '+'
+ROAD_HORIZONTAL = '-'
+ROAD_VERTICAL = '|'
+TURN_BACK = '\\'
+TURN_FORWARD = '/'
+
+class Cart
+  MAPPING = {
+    '<' => :left,
+    '>' => :right,
+    '^' => :up,
+    'v' => :down
+  }
+
+  def initialize(icon, x, y)
+    @direction = MAPPING[icon]
+    @x = x
+    @y = y
+  end
+
+  attr_reader :direction, :x, :y
+end
+
+class Map
+  def lookup x, y
+    map[y][x]
+  end
+
+  def map
+    @map ||= File.read('13.txt').lines.map do |line|
+      if line[-1] == "\n"
+        line.slice(0, line.length - 1)
+      else
+        line
+      end.split('')
+    end
+  end
+
+  def extract_carts!
+    map.each_with_index.flat_map do |line, y|
+      line.each_with_index.flat_map do |char, x|
+        if Cart::MAPPING.keys.include? char
+          line[x] = {
+            '>' => '-',
+            '<' => '-',
+            '^' => '|',
+            'v' => '|'
+          }[char]
+
+          Cart.new(char, x, y)
+        end
+      end
+    end.compact
+  end
+end
+
+class Round
+  def initialize(map, carts)
+    @map = map
+    @carts = carts
+    @tick = 0
+  end
+
+  attr_reader :map, :carts
+
+  def tick!
+    binding.pry
+    @tick += 1
+  end
+end
+
+map = Map.new
+round = Round.new(map, map.extract_carts!)
+
+loop do
+  round.tick!
+rescue => e
+  puts "Part One: #{e.message}"
+  exit
+end
